6bb9466b48aea6ae1c33f8eaf41b0e65299b1007
ps-mhp <patrick.schababerle@mhp.com>
2026-02-24 11:04:31 +0100

    Create keepalive.js

---
+++ dist/sap/keepalive.js
@@ -0,0 +1,15 @@
+let keepAlive = setInterval(function() {
+  (function findAndClick(root) {
+    Array.from(root.querySelectorAll('*')).forEach(function(el) {
+      Array.from(el.childNodes).forEach(function(node) {
+        if (node.nodeType === 3 && node.textContent.trim() === 'Anmelden') {
+          console.log(new Date().toLocaleTimeString(), '- Session verlängert!');
+          el.click();
+        }
+      });
+      if (el.shadowRoot) findAndClick(el.shadowRoot);
+    });
+  })(document);
+}, 10000);
+
+console.log('Keepalive aktiv! Stop mit: clearInterval(' + keepAlive + ')');
