0995994ea7bc250e86d02735aee3a4a98c08da11
ps-mhp <patrick.schababerle@mhp.com>
2026-02-24 11:09:53 +0100

    Create index.js

---
+++ dist/sap/index.js
@@ -0,0 +1,11 @@
+(function() {
+  function loadScript(src, config) {
+    if (config) Object.assign(window, config);
+    let s = document.createElement('script');
+    s.src = src + '?t=' + Date.now();
+    document.head.appendChild(s);
+  }
+
+  loadScript('https://cdn.jsdelivr.net/gh/ps-mhp/custom-browser-scripts@main/dist/sap/keepalive.js');
+  loadScript('https://cdn.jsdelivr.net/gh/ps-mhp/custom-browser-scripts@main/dist/sap/stundenrechner.js', { SF_CONFIG: { assignmentId: '00707080' } });
+})();
